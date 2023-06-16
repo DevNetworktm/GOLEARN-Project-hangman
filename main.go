d122c91d66e3667f90172f54164806fcc026c9b0
ZacharyMasson <m.zachary@spinforeat.com>
2023-06-16 19:06:24 +0200

    add display logic

--- main.go
+++ main.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"hangman/packages/hangman"
 	"os"
+	"strings"
 )
 
 func main() {
 	game := hangman.New(8, "Golang")
 	fmt.Printf("%+v\n", game)
 
-	l, err := hangman.ReadGuess()
-	if err != nil {
-		fmt.Printf("Error reading input: %s\n", err)
-		os.Exit(1)
+	hangman.DrawWelcome()
+
+	guess := ""
+
+	for {
+		hangman.Draw(game, guess)
+
+		switch game.State {
+		case "won", "lost":
+			os.Exit(0)
+		}
+
+		l, err := hangman.ReadGuess()
+		if err != nil {
+			fmt.Printf("Error reading input: %s\n", err)
+			os.Exit(1)
+		}
+
+		guess = strings.ToUpper(l)
 	}
-	fmt.Printf("You guessed: %s\n", l)
+
 }
